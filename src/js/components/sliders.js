b81fbeabc38495d95cd87a292f27843743dff367
ministrov <61511030+ministrov@users.noreply.github.com>
2022-08-03 12:05:56 +0300

    Worked on swiper slider to fix the problem

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -2,15 +2,27 @@ import Swiper from 'swiper';
 
 const bodyStyle = window.getComputedStyle(document.body);
 const gap = parseInt(bodyStyle.getPropertyValue('--grid-gap'));
+const portSlide = document.querySelector('.portfolio-section__items');
 
-const swiper = new Swiper('.swiper', {
+const portfolioSlider = new Swiper('.portfolio-section__items', {
   slidesPerView: 3,
   spaceBetween: gap,
   loop: true,
+  on: {
+    init: function () {
+      console.log('swiper initialized');
+
+      const activeSlide = portSlide.querySelector('.swiper-slide-active');
+
+      console.log(activeSlide);
+    },
+  },
   navigation: {
     nextEl: '.portfolio-section__next',
     prevEl: '.portfolio-section__prev'
   },
 });
 
-console.log(gap, swiper);
+portfolioSlider.on('slideChange', function () {
+  console.log('slide changed');
+});
