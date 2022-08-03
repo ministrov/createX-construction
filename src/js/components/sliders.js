979e3d6ecd2c02988e01f898e1fc4e6d6f2433e3
ministrov <61511030+ministrov@users.noreply.github.com>
2022-08-03 15:13:28 +0300

    Finished styling portfolio section

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -4,17 +4,24 @@ const bodyStyle = window.getComputedStyle(document.body);
 const gap = parseInt(bodyStyle.getPropertyValue('--grid-gap'));
 const portSlide = document.querySelector('.portfolio-section__items');
 
-const portfolioSlider = new Swiper('.portfolio-section__items', {
+const portfolioSlider = new Swiper(portSlide, {
   slidesPerView: 3,
   spaceBetween: gap,
-  loop: true,
   on: {
     init: function () {
       console.log('swiper initialized');
 
       const activeSlide = portSlide.querySelector('.swiper-slide-active');
 
-      console.log(activeSlide);
+      const nextActiveSlide = activeSlide.nextElementSibling;
+
+      const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+      activeSlide.classList.add('slider-visible');
+
+      nextActiveSlide.classList.add('slider-visible');
+
+      nextNextActiveSlide.classList.add('slider-visible');
     },
   },
   navigation: {
@@ -25,4 +32,20 @@ const portfolioSlider = new Swiper('.portfolio-section__items', {
 
 portfolioSlider.on('slideChange', function () {
   console.log('slide changed');
+
+  const activeSlide = portSlide.querySelector('.swiper-slide-next');
+
+  const nextActiveSlide = activeSlide.nextElementSibling;
+
+  const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+  activeSlide.classList.add('slider-visible');
+
+  nextActiveSlide.classList.add('slider-visible');
+
+  nextNextActiveSlide.classList.add('slider-visible');
+
+  document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+    el.classList.remove('slider-visible');
+  })
 });
