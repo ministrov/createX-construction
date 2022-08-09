0bb9325cf4598aff5f840279c80a40402f55b8c7
ministrov <61511030+ministrov@users.noreply.github.com>
2022-08-09 23:12:53 +0300

    Started working on section benefit on page services inner

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -1,10 +1,11 @@
 import GraphAccordion from '../vendor/graph-accordion'
 
-const accordion1 = new GraphAccordion('.accordion-1', {
-  speed: 400,
-});
+if (document.querySelector('.we-offer')) {
+  const accordion1 = new GraphAccordion('.accordion-1', {
+    speed: 400,
+  });
 
-const accordion2 = new GraphAccordion('.accordion-2', {
-  speed: 400
-});
-
+  const accordion2 = new GraphAccordion('.accordion-2', {
+    speed: 400
+  });
+}
