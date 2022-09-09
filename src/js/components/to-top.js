138d9b5e7e8263fbdeb4d149573141e116c81d20
ministrov <61511030+ministrov@users.noreply.github.com>
2022-09-09 13:39:06 +0300

    Ended up with scroll up

--- src/js/components/to-top.js
+++ src/js/components/to-top.js
@@ -1,3 +1,31 @@
-const dd = 'hell0';
+import SmoothScroll from '../vendor/smooth-scroll.min';
 
-console.log(dd);
+const scroll = new SmoothScroll('.to-top');
+const toTop = document.querySelector('.scroll-up');
+let heroHeight;
+
+if (document.querySelector('.hero')) {
+  heroHeight = document.querySelector('.hero').offsetHeight;
+}
+
+if (document.querySelector('.page-hero')) {
+  heroHeight = document.querySelector('page-hero').offsetHeight;
+}
+
+const isScrollUpVisible = (y = 0) => {
+  if (y >= heroHeight) {
+    toTop.classList.add('scroll-up--active');
+  } else {
+    toTop.classList.remove('scroll-up--active');
+  }
+}
+
+isScrollUpVisible(window.scrollY);
+
+window.addEventListener('scroll', () => {
+  let y = window.scrollY;
+
+  isScrollUpVisible(y);
+})
+
+
