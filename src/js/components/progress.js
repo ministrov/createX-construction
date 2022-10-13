7055dea04a117f1d8d137ccb48341f662b0e4523
ministrov <61511030+ministrov@users.noreply.github.com>
2022-10-13 15:17:55 +0300

    feat: add burger

--- src/js/components/progress.js
+++ src/js/components/progress.js
@@ -1,16 +1,3 @@
-// const circle = document.querySelector('.progress');
-
-// const progressAnimation = () => {
-//   let percentageProgress = Math.floor(70);
-
-//   let radius = circle.getAttribute('r');
-//   let circleLength = 2 * Math.PI * radius;
-//   circle.setAttribute('stroke-dasharray', circleLength);
-//   circle.setAttribute('stroke-dashoffset', circleLength - circleLength * percentageProgress / 100);
-// };
-
-// progressAnimation();
-
 const circle = document.querySelectorAll('.facts-element__circle')
 circle.forEach(el => {
   if (el.dataset.percentage == 'true') {
