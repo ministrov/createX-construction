58c5ba8f9566a958a282f5b8ce3ce3aeaa854e67
ministrov <61511030+ministrov@users.noreply.github.com>
2022-10-17 23:08:56 +0300

    fix: edit ex.js

--- src/js/components/ex.js
+++ src/js/components/ex.js
@@ -47,3 +47,53 @@ const printNumbers = (lastNumber) => {
 }
 
 printNumbers(4);
+
+const normalizeUrl = (url) => {
+  if (!url.startsWith('https://')) {
+    return `https://${url}`;
+  } else {
+    return url;
+  }
+};
+
+normalizeUrl('yandex.ru');
+
+// BEGIN (write your solution here)
+const convertText1 = (str) => {
+  const firstCharUpperCase = str.charAt(0).toUpperCase();
+  if (str === '') {
+    return '';
+  } else if (firstCharUpperCase !== str[0]) {
+    return reverse(str);
+  } else {
+    return str;
+  }
+};
+
+// with ternary operator
+const convertText2 = (str) => {
+  const firstCharUpperCase = str.charAt(0).toUpperCase();
+
+  return firstCharUpperCase !== str[0] ? reverse(str) : str;
+};
+
+console.log(convertText2);
+// END
+convertText1();
+console.log(convertText1());
+
+// Модифицируйте функцию printNumbers() так, чтобы она выводила числа в обратном порядке. Для этого нужно идти от верхней границы к нижней. То есть счётчик должен быть инициализирован максимальным значением, а в теле цикла его нужно уменьшать до нижней границы.
+
+
+const printNumbers = (/** @type {number} */ initialNumber) => {
+  // BEGIN (write your solution here)
+  let i = initialNumber;
+
+  while (i >= 1) {
+    console.log(i);
+    i -= 1;
+  }
+  console.log('finished!');
+};
+
+printNumbers(6);
