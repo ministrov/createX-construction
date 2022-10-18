f395afc8ca4a30fd16c53c86ef83f7de107718c4
ministrov <61511030+ministrov@users.noreply.github.com>
2022-10-18 22:02:35 +0300

    fix: edit ex.js

--- src/js/components/ex.js
+++ src/js/components/ex.js
@@ -33,21 +33,6 @@ const getNumberExplanation2 = (number) => {
   }
 }
 
-
-// Модифицируйте функцию printNumbers() так, чтобы она выводила числа в обратном порядке. Для этого нужно идти от верхней границы к нижней. То есть счётчик должен быть инициализирован максимальным значением, а в теле цикла его нужно уменьшать до нижней границы.
-
-const printNumbers = (lastNumber) => {
-  let i = lastNumber;
-
-  while (i >= 0) {
-    console.log(i);
-    i -= 1;
-  }
-  console.log('finished!');
-}
-
-printNumbers(4);
-
 const normalizeUrl = (url) => {
   if (!url.startsWith('https://')) {
     return `https://${url}`;
