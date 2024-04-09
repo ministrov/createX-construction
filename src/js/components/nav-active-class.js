2b5243d921ce8c41d9b903ecc2f4d8a3439a8700
unknown <ministrov2018@gmail.com>
2024-04-09 14:10:12 +0300

    feat: add an active class change  to header navigation

---
+++ src/js/components/nav-active-class.js
@@ -0,0 +1,19 @@
+const navItems = document.querySelectorAll('.nav__item');
+const links = navItems.querySelectorAll('.nav__item-link');
+
+console.log(navItems);
+console.log(navItems.childNodes);
+
+// navItems.childNodes.forEach(item => {
+//   item.addEventListener('click', function(e) {
+//     console.log(e.target);
+//   })
+// });
+console.log(typeof navItems);
+
+// const getSum = (a, b) => {
+//   return a + b;
+// }
+
+// console.log(12, 5);
+
