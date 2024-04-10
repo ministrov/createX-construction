060546c6776ecb2ed5a65384d661906028a24fe7
unknown <ministrov2018@gmail.com>
2024-04-10 10:35:33 +0300

    feat: start adding new animation to header nav handling active class

--- src/js/components/nav-active-class.js
+++ src/js/components/nav-active-class.js
@@ -2,9 +2,12 @@ const navLinks = document.querySelectorAll('.nav__item-link');
 const windowPathname = window.location.pathname;
 
 navLinks.forEach(link => {
-  if (link.href.includes(windowPathname)) {
+  const navLinkPathname = new URL(link.href).pathname;
+
+  console.log(navLinkPathname);
+  if (windowPathname === navLinkPathname) {
     link.classList.add('nav__item-link--current');
   }
 });
 
-// console.log(window.location);
+console.log(window.location);
